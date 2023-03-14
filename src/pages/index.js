6f48f84425b8ee896be13c79aaa7a947f8ddf02a
tim <dzhoroev1@gmail.com>
2023-03-14 14:13:55 +0900

    feat: gatsby link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Link } from "gatsby";
 
 const IndexPage = () => {
   return (
     <div>
       <h1>Hello.</h1>
       <h2>I'm Tim, a front-end devloper living in beautiful Seoul.</h2>
+      <p>
+        <a href="/contact"> Need developer? </a>
+      </p>
+      <p>
+        <Link to="/contact"> Need developer? </Link>
+      </p>
     </div>
   );
 };
