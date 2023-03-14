ce6a86c82fa3385a73d5ba8a652f7a5106d330e7
tim <dzhoroev1@gmail.com>
2023-03-14 15:16:13 +0900

    feat: scss modules

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,15 @@
 import { Link } from "gatsby";
 import React from "react";
 
+import * as styles from "./header.module.scss"
+
 const Header = () => {
   return (
     <header>
         <h1>Tims, front-end dev.</h1>
       <ul>
         <li>
-          <Link to="/">Main</Link>
+          <Link className={`${styles.main}`} to="/">Main</Link>
         </li>
         <li>
           <Link to="/about">About me</Link>
