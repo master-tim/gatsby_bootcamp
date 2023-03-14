22e8a7b9f99f9b557c8da55bb01b9d59d26886bd
tim <dzhoroev1@gmail.com>
2023-03-14 15:52:42 +0900

    style: code prettify

--- src/components/header.js
+++ src/components/header.js
@@ -7,28 +7,44 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <h1>
-        <Link className={styles.title}  to="/">
+        <Link className={styles.title} to="/">
           Temirlan Dzhoroev
         </Link>
       </h1>
-      <ul className={styles.navList} >
+      <ul className={styles.navList}>
         <li>
-          <Link className={styles.navItem} activeClassName={styles.activeNavItem} to="/">
+          <Link
+            className={styles.navItem}
+            activeClassName={styles.activeNavItem}
+            to="/"
+          >
             Home
           </Link>
         </li>
         <li>
-          <Link className={styles.navItem} activeClassName={styles.activeNavItem} to="/about">
+          <Link
+            className={styles.navItem}
+            activeClassName={styles.activeNavItem}
+            to="/about"
+          >
             About me
           </Link>
         </li>
         <li>
-          <Link className={styles.navItem} activeClassName={styles.activeNavItem} to="/blog">
+          <Link
+            className={styles.navItem}
+            activeClassName={styles.activeNavItem}
+            to="/blog"
+          >
             Blog
           </Link>
         </li>
         <li>
-          <Link className={styles.navItem} activeClassName={styles.activeNavItem} to="/contact">
+          <Link
+            className={styles.navItem}
+            activeClassName={styles.activeNavItem}
+            to="/contact"
+          >
             Contact
           </Link>
         </li>
