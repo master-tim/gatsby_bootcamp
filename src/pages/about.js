19c1b70a34df8872639a75061c934945d77b6548
tim <dzhoroev1@gmail.com>
2023-03-15 13:39:50 +0900

    chore: about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,29 @@
-import { Link } from "gatsby";
+import { Link, graphql, useStaticQuery } from "gatsby";
 import React from "react";
 import Layout from "../components/layout";
 import Head from "../components/head";
 
 const AboutPage = () => {
+  const data = useStaticQuery(graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        author
+        name
+      }
+    }
+  }
+  `);
   return (
     <Layout>
       <Head title="About me" />
-      <h1>Tim</h1>
-      <h2>I'm Tim, a front-end devloper living in beautiful Seoul.</h2>
+      <h1>{data.site.siteMetadata.author}</h1>
+      <p>
+        I am keen on pursuing opportunities in the field of user experience and
+        web development. My goal is to use my design and technical skills to
+        create intuitive, accessible, and engaging digital products.
+      </p>
       <p>
         <Link to="/contact">Contact me</Link>
       </p>
