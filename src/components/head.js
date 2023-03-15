1bdf75ec2a19b54fe9a79e30acf823a6dd2710e6
tim <dzhoroev1@gmail.com>
2023-03-15 12:25:43 +0900

    fix: helmet

--- src/components/head.js
+++ src/components/head.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Helmet } from "react-helmet";
 
 const Head = () => {
-  return <Head title="This is a test" />;
+  return <Helmet title="This is a test" />;
 };
 
 export default Head;
