338322720eecec8ce3d360ac5b19381789c2b287
tim <dzhoroev1@gmail.com>
2023-03-16 12:00:38 +0900

    feat: dark light mode

--- src/components/header.js
+++ src/components/header.js
@@ -23,9 +23,62 @@ import {
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 
-function Header() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+const Indexes = (data) => {
+  return (
+    <HStack spacing={8} alignItems={"center"} marginLeft="1rem">
+      <Heading
+        px={2}
+        py={1}
+        rounded={"md"}
+        _hover={{
+          textDecoration: "none",
+          bg: useColorModeValue("gray.200", "gray.700"),
+        }}
+      >
+        <Link to="/">DT</Link>
+      </Heading>
+      <HStack as={"nav"} spacing={4} display={{ base: "none", md: "flex" }}>
+        <Text
+          px={2}
+          py={1}
+          rounded={"md"}
+          _hover={{
+            textDecoration: "none",
+            bg: useColorModeValue("gray.200", "gray.700"),
+          }}
+        >
+          <Link to="/about">About</Link>å
+        </Text>
+        <Text
+          px={2}
+          py={1}
+          rounded={"md"}
+          _hover={{
+            textDecoration: "none",
+            bg: useColorModeValue("gray.200", "gray.700"),
+          }}
+        >
+          <Link to="/blog">Blogs</Link>
+        </Text>
+        <Text
+          px={2}
+          py={1}
+          rounded={"md"}
+          _hover={{
+            textDecoration: "none",
+            bg: useColorModeValue("gray.200", "gray.700"),
+          }}
+        >
+          <Link to="/contact">Contact</Link>
+        </Text>
+      </HStack>
+    </HStack>
+  );
+};
 
+function Header() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const data = useStaticQuery(graphql`
     query {
       site {
@@ -50,67 +103,11 @@ function Header() {
             display={{ md: "none" }}
             onClick={isOpen ? onClose : onOpen}
           />
-
-          <HStack spacing={8} alignItems={"center"} marginLeft="1rem">
-            <Heading
-              px={2}
-              py={1}
-              rounded={"md"}
-              _hover={{
-                textDecoration: "none",
-                bg: useColorModeValue("gray.200", "gray.700"),
-              }}
-            >
-              <Link to="/">{data.site.siteMetadata.nickname}</Link>
-            </Heading>
-            <HStack
-              as={"nav"}
-              spacing={4}
-              display={{ base: "none", md: "flex" }}
-            >
-              <Text
-                px={2}
-                py={1}
-                rounded={"md"}
-                _hover={{
-                  textDecoration: "none",
-                  bg: useColorModeValue("gray.200", "gray.700"),
-                }}
-              >
-                <Link to="/about">About</Link>
-              </Text>
-              <Text
-                px={2}
-                py={1}
-                rounded={"md"}
-                _hover={{
-                  textDecoration: "none",
-                  bg: useColorModeValue("gray.200", "gray.700"),
-                }}
-              >
-                <Link to="/blog">Blogs</Link>
-              </Text>
-              <Text
-                px={2}
-                py={1}
-                rounded={"md"}
-                _hover={{
-                  textDecoration: "none",
-                  bg: useColorModeValue("gray.200", "gray.700"),
-                }}
-              >
-                <Link to="/contact">Contact</Link>
-              </Text>
-            </HStack>
-          </HStack>
-
+          <Indexes />
           <Flex alignItems={"center"} marginRight="1rem">
-            <Avatar
-              size={"sm"}
-              src={
-                "https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
-              }
-            />
+            <Button onClick={toggleColorMode}>
+              {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+            </Button>
           </Flex>
         </Flex>
 
